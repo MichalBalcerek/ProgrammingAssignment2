cf50efffbbc6cbaa91e0ae99ed6eadd0cd863e43
MichalBalcerek <mbalcerek@poczta.onet.pl>
2015-10-24 12:35:44 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function makeCacheMatrix makes a "special" matrix with the ability 
+## to store its inverse.
+## You can access the cached inverse by using $getInverse() on your "special" matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv
+  getInverse <- function() inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Using function cacheSolve checks if the inverse is already calculated and stored,
+## otherwise it calculates the inverse (after checking if it is possible).
+## If the inverse is already cached - it does not calculate it again - it returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  # in the above "if" we check if the matrix can be invertible - if it is square 
+  # matrix of equal dimension greater than 1 (first condition),
+  # or if it is just a single number (condition after ||)
+  if ((class(data)=="matrix" && dim(data)[1]==dim(data)[2]) || length(data)==1){
+    inverse <- solve(data)
+  }else{ 
+    message("Cannot compute inverse of matrix - invalid dimensions")}
+  x$setInverse(inverse)
+  inverse
 }
